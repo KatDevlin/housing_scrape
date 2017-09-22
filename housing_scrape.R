da6abdce49a25220292b17d7f0a1d133723aef25
KatDev <kathleen.devlin@yale.edu>
2017-09-22 11:48:09 -0400

    Update code

---
+++ housing_scrape.R
@@ -0,0 +1,44 @@
+# New Haven Housing Scrape Redux
+
+# Adaptation from a homework assignment. I know there are R 
+# packages to handle scraping, but for class we were not permitted
+# to use outside packages for this exercise. 
+
+# setwd("/Users/katdevlin/Desktop/housing_scrape")
+
+this <- file.path("http://gis.vgsi.com/newhavenct/",
+                  "Parcel.aspx?Pid=1")
+z <- scan(this, what="", sep="\n")
+
+# If we look at `z` we can see the contents from the first database entry 
+# were successfully puled in. Now let's scrape a bunch.
+
+baseurl <- file.path("http://gis.vgsi.com/newhavenct/",
+                     "Parcel.aspx?Pid=XXX")
+
+x <- data.frame(Assessment=rep(NA, 100), Bedrooms=rep(NA,100),
+                LvngArea=rep(NA,100))
+
+for (i in 1:100) {
+  thisurl <- gsub("XXX", i, baseurl)
+  z <- scan(thisurl, what="", sep="\n")
+  temp <- z[grep(".*GenAssessment.*</dd>", z)]
+  temp <- gsub(".*\\$([0-9,]+).*", "\\1", temp)
+  temp <- gsub(",","", temp)
+  temp2 <- z[grep("Bed", z, fixed=TRUE)]
+  temp2 <- gsub(".*([0-9]+).*", "\\1", temp2)
+  temp3 <- z[grep("BldArea", z, fixed=TRUE)]
+  temp3 <- gsub(".*>([0-9]+).*", "\\1", temp3)
+  x$Assessment[i] <- as.numeric(temp)
+  x$Bedrooms[i] <- as.numeric(temp2)
+  x$LvngArea[i] <- as.numeric(temp3)
+} 
+
+write.csv(x, file="housing_scrape.csv")
+
+head(x)
+tail(x)
+#
+for (i in 1:ncol(x)) {
+  print(str(x[,i])) 
+}
